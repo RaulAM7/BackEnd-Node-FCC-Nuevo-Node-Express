52fad96ba959193f549f7ace292ef40470fb1078
raulAM7 <raulartilesm@gmail.com>
2024-12-07 18:55:57 +0000

    update

--- myApp.js
+++ myApp.js
@@ -5,6 +5,8 @@ let app = express();
 // Class 1 - Meet the Node console
 console.log('Hello world')
 
+console.log('this is a change using nodemon')
+console.log('this is antoher change using nodemon')
 
 
 
