53f8e305a02754e1c33aee4f047fff1318184831
raulAM7 <raulartilesm@gmail.com>
2024-12-07 22:08:06 +0000

    update: understood all types of responses

--- myApp.js
+++ myApp.js
@@ -10,16 +10,32 @@ console.log('this is antoher change using nodemon')
 
 
 
-// Class 1 - Meet the Node console
+// Class 2 - Start a working Express server
 app.get("/", (req, res)=> {
     res.send('Hello Express')
 })
 
 
-
-
-
-
+// Class 3 - Route handlers responses types
+app.get("/send-1", (req, res) => {
+    res.send('Esto es un texto mandado por .send')
+})
+app.get("/send-2", (req, res ) => {
+    res.send('<h1>This is an h1 sent by the .send method of the handler of this route</h1>')
+})
+app.get("/json-1", (req, res)=> {
+    res.json( {"name": "Raul"} )
+})
+app.get("/sendFile-1", (req, res) => {
+    const filePath = __dirname + "/views/index.html"
+    res.sendFile(filePath)
+})
+app.get("/redirect", (req, res) => {
+    res.redirect("/redirected")
+})
+app.get("/redirected", (req, res) => {
+    res.send("You have been redirected")
+})
 
 
 module.exports = app
