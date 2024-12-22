ac32bf8e97013717455dc4700d06ba99f4fff81c
raulAM7 <raulartilesm@gmail.com>
2024-12-22 14:16:05 +0000

    update

--- RepasoAgain/myApp.js
+++ RepasoAgain/myApp.js
@@ -5,6 +5,11 @@ console.log('Hello World')
 
 
 
+app.use("/public", express.static(__dirname + "/public"))
+
+
+
+
 app.get("/", (req,res) => {
     console.log('Hello Express')
     let absolutePath = __dirname + "/views/index.html"
