04ed3099db270800abdebc4485e9add826d5ad73
raulAM7 <raulartilesm@gmail.com>
2024-12-22 14:18:13 +0000

    update

--- RepasoAgain/myApp.js
+++ RepasoAgain/myApp.js
@@ -1,22 +1,31 @@
+/*
+----------- DEPENDENCIES
+*/
 let express = require('express')
 let app = express()
 
 console.log('Hello World')
 
 
+/*
+----------- MIDDLEWARE
+*/
 
-app.use("/public", express.static(__dirname + "/public"))
+app.use('/public', express.static(__dirname + "/public"))
 
-
-
+/*
+----------- ROUTES
+*/
 
 app.get("/", (req,res) => {
-    console.log('Hello Express')
     let absolutePath = __dirname + "/views/index.html"
     res.sendFile(absolutePath)
 })
 
-
+app.get("/json", (req, res) => {
+    let message = 'Hello json'
+    res.json({"message": message})
+})
 
 
 module.exports = app
