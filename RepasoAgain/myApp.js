cb711c5271e9f7ffcfee97d7f01a07a1f8eaa3d6
raulAM7 <raulartilesm@gmail.com>
2024-12-22 15:14:14 +0000

    update

--- RepasoAgain/myApp.js
+++ RepasoAgain/myApp.js
@@ -1,15 +1,38 @@
 /*
------------ DEPENDENCIES
+----------- .ENV FILE
+*/
+
+let dotenv = require('dotenv').config()
+console.log(process.env.MESSAGE_STYLE)
+
+/*
+----------- EXPRESS APP OBJECT
 */
 let express = require('express')
 let app = express()
 
-console.log('Hello World')
+/*
+----------- DEPENDENCIES
+*/
 
 
 /*
 ----------- MIDDLEWARE
 */
+app.use((req, res, next) => {
+    console.log('Alright, alright, alright')
+    next()
+})
+
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.path} - ${req.ip}:${req.socket.localPort}`)
+    if (req.socket.localPort === 3000)
+    {
+        console.log('Estamos en casita en el puerto 3000')
+    }
+    next()
+})
+
 
 app.use('/public', express.static(__dirname + "/public"))
 
@@ -24,8 +47,28 @@ app.get("/", (req,res) => {
 
 app.get("/json", (req, res) => {
     let message = 'Hello json'
-    res.json({"message": message})
+    if (process.env.MESSAGE_STYLE === 'uppercase')
+    {
+        message = message.toUpperCase()
+    }
+    else
+    {
+        message = message.toLowerCase()
+    }
+    res.json({message: message})
 })
+
+
+app.get("/now", (req, res, next) => {
+    req.time = new Date().toString()
+    next()
+    }, (req, res) => {
+        res.json({time: req.time})
+    }
+)
+
+
+
 
 
 module.exports = app
